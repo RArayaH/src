1ae64bf471ab4a4d4cd20327331a1b7dea1933eb
RArayaH <81941016+RArayaH@users.noreply.github.com>
2021-11-17 13:19:45 -0300

    src proyecto

---
+++ app/Pages/info/info.page.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { InfoUsuario } from 'src/app/interfaces/info-usuario';
+import { APIClientService } from 'src/app/services/apiclient.service';
+import { BdLocalService } from 'src/app/services/bd-local.service';
+
+
+@Component({
+  selector: 'app-info',
+  templateUrl: './info.page.html',
+  styleUrls: ['./info.page.scss'],
+})
+export class InfoPage { 
+
+  data:InfoUsuario[]=[];
+
+  constructor(private bdLocalService: BdLocalService, private api:APIClientService) {
+    this.mostrarDatos();
+  }
+  
+  mostrarDatos(){
+    this.api.getData().subscribe(data => {
+      console.log(data);
+    })
+  }
+
+  cerrarSesion(){
+    this.bdLocalService.cerrarSesion();
+    }
+  
+  
+    
+}
